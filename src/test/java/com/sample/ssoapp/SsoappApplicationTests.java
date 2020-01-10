13fbd8fdcdfc84c84279d6122769e880d6e35897
SaurabhTiwari <saurabhtiwari@fico.com>
2020-01-10 14:49:14 +0530

    initial commit

---
+++ src/test/java/com/sample/ssoapp/SsoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.ssoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
